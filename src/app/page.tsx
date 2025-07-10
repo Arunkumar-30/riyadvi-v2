4f771321f634e7e5bf57781cd327525cc4a3ecf1
Arunkumar-30 <Selvam.arun100@gmail.com>
2025-07-10 16:34:21 +0530

    df

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import AboutComponent from "@/components/ui-section/About";
 import BannerSlider from "@/components/ui-section/Banner";
 import CaseStudies from "@/components/ui-section/CaseStudies";
 import ServiceHome from "@/components/ui-section/ServiceHome";
-import TestimonialSlider from "@/components/ui-section/Testimonal";
 import Testimonials from "@/components/ui-section/Testimonal";
 import TrustedPartner from "@/components/ui-section/TrustedPartners";
-import WhyChooseUsComponent from "@/components/ui-section/WhyChooseUs";
 
 export default function Home() {
   return (
